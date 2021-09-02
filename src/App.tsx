6e77f99d503d66d030f21b0d9c36885201fa97d9
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-01 23:17:00 -0300

    Inclusão do PeopleContext no app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 // import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { PeopleContextProvider } from './contexts/PeopleoContext';
 import { Home } from './pages/Home';
+import { People } from './pages/People';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home} />
+          <PeopleContextProvider>
+            <Route path="/" exact component={Home} />
+          </PeopleContextProvider>
         </Switch>
     </BrowserRouter>
   );
