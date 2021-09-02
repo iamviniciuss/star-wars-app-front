0be1424b5cbdb976bc5ae3e792d87f44413fe377
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-01 23:16:03 -0300

    Transformação da Home em componentes.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,221 +1,45 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import './index.css'
 import { Carousel } from './components/Carousel'
+import { Nav } from '../components/Nav'
+import ModalPeople from './components/ModalPeople'
+import { CardPeople } from './components/CardPeople'
+import { useEffect, useState } from 'react'
+import Swapi from '../../services/api'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://www.fotoefeitos.com/images/202109/01/fotoefeitos.com__final_1480036530360780018_.jpg?act=91',
-}
-const navigation = [
-  { name: 'Peoples', href: '#', current: true },
-  { name: 'Planets', href: '#', current: false },
-  { name: 'Species', href: '#', current: false },
-  { name: 'Starcrafts', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Sign out', href: '#' },
-]
-
-const peoples = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-]
-
-function classNames(...classes:any) {
-  return classes.filter(Boolean).join(' ')
-}
 
 export function Home() {
-  return (
-    <div>
-      <Disclosure as="nav" className="bg-black">
-        {({ open }) => (
-          <>
-            <div className="mx-auto lg:mx-6 px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-20 w-20"
-                      src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden md:block">
-                  <div className="ml-4 flex items-center md:ml-6">
-                    <button
-                      type="button"
-                      className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
 
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="ml-3 relative">
-                      <div>
-                        <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                          <span className="sr-only">Open user menu</span>
-                          <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a
-                                  href={item.href}
-                                  className={classNames(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
-                                  )}
-                                >
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  </div>
-                </div>
-                <div className="-mr-2 flex md:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
+    const [peoples , setPeoples] = useState([]);
 
-            <Disclosure.Panel className="md:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="pt-4 pb-3 border-t border-gray-700">
-                <div className="flex items-center px-5">
-                  <div className="flex-shrink-0">
-                    <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                    <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
-                  </div>
-                  <button
-                    type="button"
-                    className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mt-3 px-2 space-y-1">
-                  {userNavigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+    useEffect(()=>{
+        Swapi.listPeoples()
+            .then((response:any) => {
+                setPeoples(response.data);
+            })
+    },[])
 
-        <Carousel/>  
- 
-        <main>
-            <div className="mx-auto lg:mx-4 py-6 sm:px-6 lg:px-8 ">
-                <div className="px-4 py-6 sm:px-0">
-                    <div className="rounded-lg flex flex-wrap items-center justify-center" >
-                        {
-                            peoples.map((people:any) => (
+    return (
+        <div>
+            <Nav/>
+            
+            <Carousel/>
 
-                                <div className="hover:bg-yellow-400  bg-gray-100 bg-opacity-60 shadow-md overflow-hidden flex flex-wrap items-center justify-center card-list m-1">
-                                    <div className="vacation-card">
-                                        <div className="vacation-card-info">
-                                            <div>
-                                            <div className="vacation-card-eyebrow">
-                                                <p className="text-sm">Luke Skywalker</p>
-                                            </div>
-                                            <div className="vacation-card-title">
-                                                <a href="/vacations/cancun"></a>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+            <ModalPeople />
 
-                            ))
-                        }
+            <main>
+                <div className="mx-auto lg:mx-4 py-6 sm:px-6 lg:px-8 ">
+                    <div className="px-4 py-6 sm:px-0">
+                        <div className="rounded-lg flex flex-wrap items-center justify-center" >
+                            {
+                                peoples.map((people:any) => (
+                                    <CardPeople people={people}/>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
-    </div>
-  )
+            </main>
+
+        </div>
+    )
 }
