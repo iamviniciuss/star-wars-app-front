88045f6f55db6bf04ee9f02dc992872666fd3bd5
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-01 23:18:17 -0300

    Remoção do componente People do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-// import React from 'react';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { PeopleContextProvider } from './contexts/PeopleoContext';
 import { Home } from './pages/Home';
-import { People } from './pages/People';
 
 function App() {
   return (
