9b87d0f15904a298b492220db604e2fa6f2415fe
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-05 20:38:54 -0300

    Inclusão de atributos com tipagem e queryString para paginação.

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,22 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.94.0.1:3000'
-    
+    baseURL: 'http://localhost:3000'
 });
 
 const options = {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
+    headers: {}
 }
 
 const Swapi = {
-    listPeoples: async (limit:Number = 10 , offset:Number = 0)=>{
-        return api.get(`/api/v1/peoples/?limit=${limit}&offset=${offset}`, options )
+    listPeoples: async (limit:number = 10 , offset:number = 0)=>{
+        return api.get(`/api/v1/peoples/?limit=${limit}&offset=${(offset * limit)}`, options )
     },
-    getPeople: async (people_id:Number = 0)=>{
+    getPeople: async (people_id:number = 0)=>{
         return api.get(`/api/v1/peoples/${people_id}`, options )
     }
 }
