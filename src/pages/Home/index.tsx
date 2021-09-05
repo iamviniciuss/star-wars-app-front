7e4b224b936fe8513a7d60cf59f179e5eb174270
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-05 20:36:32 -0300

    Inclusão de novos componentes e Context.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,43 +3,50 @@ import { Carousel } from './components/Carousel'
 import { Nav } from '../components/Nav'
 import ModalPeople from './components/ModalPeople'
 import { CardPeople } from './components/CardPeople'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Swapi from '../../services/api'
+import Navigation from './components/Navigation'
+import { HomePageContext } from '../../contexts/HomeContext'
 
 
 export function Home() {
 
     const [peoples , setPeoples] = useState([]);
 
+    const { offsetNavigation } = useContext(HomePageContext)
+
     useEffect(()=>{
-        Swapi.listPeoples()
+        
+        Swapi.listPeoples(10 , offsetNavigation)
             .then((response:any) => {
                 setPeoples(response.data);
             })
-    },[])
+
+    },[offsetNavigation])
 
     return (
-        <div>
-            <Nav/>
-            
-            <Carousel/>
+            <>
+                <Nav/>
+                
+                <Carousel/>
 
-            <ModalPeople />
+                <ModalPeople />
 
-            <main>
-                <div className="mx-auto lg:mx-4 py-6 sm:px-6 lg:px-8 ">
-                    <div className="px-4 py-6 sm:px-0">
-                        <div className="rounded-lg flex flex-wrap items-center justify-center" >
-                            {
-                                peoples.map((people:any) => (
-                                    <CardPeople people={people}/>
-                                ))
-                            }
+                <main>
+                    <div className="mx-auto lg:mx-4 py-6 sm:px-6 lg:px-8 ">
+                        <div className="px-4 py-6 sm:px-0">
+                            <Navigation />
+                            <div className="rounded-lg flex flex-wrap items-center justify-center" >
+                                {
+                                    peoples.map((people:any) => (
+                                        <CardPeople people={people}/>
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
-                </div>
-            </main>
+                </main>
 
-        </div>
+            </>
     )
 }
